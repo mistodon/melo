4a1d3015fb2f39aa73358b2da682d1a6d4b7d02e
Claire Harris <pirh.badger@gmail.com>
2018-01-23 02:59:10 +0000

    Add notes and instruments reference subcommands

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,34 @@ use structopt::StructOpt;
 
 
 #[derive(Debug, StructOpt)]
-struct Command
+enum Command
 {
-    #[structopt(help = "Input file, or stdin")]
-    input: Option<String>,
+    #[structopt(name = "abc", about = "Compile midscript to abc notation.")]
+    Abc
+    {
+        #[structopt(help = "Input file, or stdin if not specified.")]
+        input: Option<String>,
 
-    #[structopt(short = "o", long = "output", help = "Output file, or stdout")]
-    output: Option<String>,
+        #[structopt(short = "o", long = "output", help = "Output file, or stdout if not specified.")]
+        output: Option<String>,
+    },
+
+    #[structopt(name = "ref", about = "View useful information for composing in miscript.")]
+    Ref
+    {
+        #[structopt(subcommand)]
+        subcommand: RefCommand,
+    }
+}
+
+#[derive(Debug, StructOpt)]
+enum RefCommand
+{
+    #[structopt(name = "notes", about = "View information about valid notes.")]
+    Notes,
+
+    #[structopt(name = "instruments", about = "View the program numbers for GM instruments.")]
+    Instruments,
 }
 
 
@@ -25,44 +46,61 @@ fn main()
 
     let command = Command::from_args();
 
-    let input_text = {
-        use std::io::Read;
-
-        let mut content = String::new();
-
-        match command.input
+    match command
+    {
+        Command::Abc { input, output } =>
         {
-            Some(filename) => {
+            let input_text = {
+                use std::io::Read;
 
-                File::open(&filename).unwrap().read_to_string(&mut content).unwrap();
-            },
-            None => {
-                std::io::stdin().read_to_string(&mut content).unwrap();
+                let mut content = String::new();
+
+                match input
+                {
+                    Some(filename) => {
+
+                        File::open(&filename).unwrap().read_to_string(&mut content).unwrap();
+                    },
+                    None => {
+                        std::io::stdin().read_to_string(&mut content).unwrap();
+                    }
+                }
+
+                content
+            };
+
+            let processed = match midscript::compile_to_abc_new(&input_text)
+            {
+                Err(err) => {
+                    eprintln!("Compilation failed:\nerror: {}", err);
+                    std::process::exit(1)
+                },
+                Ok(p) => p
+            };
+
+            {
+                use std::io::Write;
+
+                if let Some(filename) = output
+                {
+                    File::create(&filename).unwrap().write_all(processed.as_bytes()).unwrap();
+                }
+                else
+                {
+                    std::io::stdout().write_all(processed.as_bytes()).unwrap();
+                }
             }
         }
 
-        content
-    };
-
-    let processed = match midscript::compile_to_abc_new(&input_text)
-    {
-        Err(err) => {
-            eprintln!("Compilation failed:\nerror: {}", err);
-            std::process::exit(1)
-        },
-        Ok(p) => p
-    };
-
-    {
-        use std::io::Write;
-
-        if let Some(filename) = command.output
+        Command::Ref { subcommand } => match subcommand
         {
-            File::create(&filename).unwrap().write_all(processed.as_bytes()).unwrap();
-        }
-        else
-        {
-            std::io::stdout().write_all(processed.as_bytes()).unwrap();
+            RefCommand::Notes => println!(
+                "{}",
+                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/reference/notes.txt"))),
+
+            RefCommand::Instruments => println!(
+                "{}",
+                include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/reference/instruments.txt"))),
         }
     }
 }
