cd1bcb6cec2c268592b55db5b0e9a71520736f96
Vi <1149443+mistodon@users.noreply.github.com>
2018-02-02 19:02:30 +0000

    Add prototype of direct-to-midi compiler

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,14 @@ extern crate lazy_static;
 
 extern crate ansi_term;
 extern crate regex;
+extern crate rimd;
 
 
 pub mod notes;
 mod abc_generation;
 mod error;
 mod lexing;
+mod midi_generation;
 mod parsing;
 mod sequencing;
 mod trust;
@@ -34,4 +36,18 @@ pub fn compile_to_abc(input: &str, filename: Option<&str>) -> Result<String, Err
     let abc = abc_generation::generate_abc(&pieces, &source_map)?;
 
     Ok(abc)
+}
+
+
+pub fn compile_to_midi(input: &str, filename: Option<&str>) -> Result<Vec<u8>, Error>
+{
+    let (tokens, source_map) = lexing::lex(input, filename)?;
+    let parse_tree = parsing::parse(&tokens, &source_map)?;
+    let pieces = sequencing::sequence_pieces(&parse_tree, &source_map)?;
+    let midi = midi_generation::generate_midi(
+        pieces.get(0).ok_or(failure::err_msg("No pieces found"))?,
+        &source_map,
+    ).ok_or(failure::err_msg("Compilation to MIDI failed!"))?;
+
+    Ok(midi)
 }
